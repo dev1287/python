2a18b1ca33025c570438e84ea281b7601ec7b606
Ravella <89604314+dev1287@users.noreply.github.com>
2024-02-06 14:14:59 +0000

    changes on feb6

--- 02_Arithmetic_Operations/e_savingsbank.py
+++ 02_Arithmetic_Operations/e_savingsbank.py
@@ -14,10 +14,23 @@ Step 5: House Rent paid of 1500
 Step 6: Display Balance
 """
 #balance = 0
-#initial_desposit = 1500
+#initial_deposit = 1500
 #salary_credited = 3300
 #online_purchase = 33.33
 #house_rent = 1500
-#current_balance= balance + initial_deposit + salary_credited - online_purchase + house_rent
+#current_balance = balance + initial_deposit + salary_credited - online_purchase + house_rent
 #print("balance:", current_balance)
-#
+
+balance = input("Enter balance:")
+balance = int(balance)
+print("balance:", balance)
+initial_deposit = input("Enter initial deposit:")
+initial_deposit = int(initial_deposit)
+salary_credited = input("salary credited amount:")
+salary_credited = int(salary_credited)
+online_purchase = input("online purchase amount:")
+online_purchase = int(online_purchase)
+house_rent = input("enter house rent:")
+house_rent = int(house_rent)
+current_balance=balance+initial_deposit+salary_credited-online_purchase+house_rent
+print("updated balance:", current_balance)
